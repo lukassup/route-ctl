a3ff2f24f85b93af18d342b9aa248c6188534fe8
Lukas Šupienis <lukas.supienis@gmail.com>
2017-04-25 16:54:34 +0300

    More tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -40,6 +40,27 @@ class netroutes::routes {               # comment
 }  # end file
 '''
 
+MISSING_HEADER_FILE = '''\
+# a comment
+network_route { '172.17.67.0/24':     # comment
+  ensure    => 'present',             # comment
+  gateway   => '10.0.2.2',            # comment
+  interface => 'eth0',                # comment
+  netmask   => '255.255.255.0',       # comment
+  network   => '172.17.67.0',         # comment
+  options   => 'table 200',           # comment
+}  # comment
+network_route { 'default':
+  ensure    => 'present',
+  # ensure    => 'absent',
+  gateway   => '10.0.2.2',
+  # gateway   => '10.0.3.3',
+  interface => $appout,
+  netmask   => '0.0.0.0',
+  network   => 'default'
+}  # end route
+'''
+
 MISSING_OPEN_BRACE_FILE = '''\
 # a comment
 class netroutes::routes                 # comment
@@ -117,6 +138,53 @@ class TestRouteParser(unittest.TestCase):
             # NOTE: there is no context manager for ``assertRaises``` in PY26.
             self.assertRaises(
                 routes.StartTokenNotFoundError,
+                routes.parse_route,  # test func
+                route_file      # arg
+            )
+
+
+class TestFindHeader(unittest.TestCase):
+    """Test `find_header()` function."""
+
+    def test_header_present(self):
+        """find_header() should stop right after header if it exists."""
+        with StringIO(VALID_ROUTE_FILE) as route_file:
+            try:
+                routes.find_header(route_file)
+            except Exception as e:
+                self.fail(e)
+            line = route_file.readline().rstrip()
+            # should read the first line after the header
+            self.assertEqual(
+                line, "  network_route { '172.17.67.0/24':     # comment")
+
+    def test_header_absent(self):
+        """find_header() should raise the correct exception if header doesn't exist."""
+        with StringIO(MISSING_HEADER_FILE) as route_file:
+            # NOTE: there is no context manager for ``assertRaises``` in PY26.
+            self.assertRaises(
+                routes.StartTokenNotFoundError,
+                routes.find_header,  # test func
+                route_file      # arg
+            )
+
+
+class TestFindClosingBrace(unittest.TestCase):
+    """Test `find_closing_brace` function."""
+
+    def test_closing_brace_present(self):
+        with StringIO(VALID_ROUTE_FILE) as route_file:
+            try:
+                routes.find_closing_brace(route_file)
+            except Exception as e:
+                self.fail(e)
+
+    def test_closing_brace_absent(self):
+        """Correct exception should be raised when missing a close brace."""
+        with StringIO(MISSING_CLOSE_BRACE_FILE) as route_file:
+            # NOTE: there is no context manager for ``assertRaises``` in PY26.
+            self.assertRaises(
+                routes.EndTokenNotFoundError,
                 routes.parse_route,  # test func
                 route_file      # arg
             )
