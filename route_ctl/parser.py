c591a59a52c07fb5fd8535e15756d53d537a5163
Lukas Šupienis <lukas.supienis@gmail.com>
2017-06-05 23:36:18 +0300

    Decouple the parser and make it OOP

---
+++ route_ctl/parser.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+
+"""Core route parsing functionality."""
+
+from __future__ import absolute_import, unicode_literals
+
+import re
+
+from . import core
+
+
+ROUTE_FILE_HEADER = re.compile(
+    r'''
+    ^(\s*)                          # indentation
+    class\ netroutes::routes\s*{    # file header token
+    [^\S\n\r]*                      # any non-line-end whitespace
+    (\#.*)?                         # maybe a comment
+    $
+    ''',
+    flags=re.VERBOSE)
+
+ROUTE_BLOCK_HEAD = re.compile(
+    r'''
+    ^(\s*)                 # indentation
+    network_route\s*{\s*   # route entry header
+    ([\'"])                # opening quote
+    (?P<name>.*?)          # value in quotes
+    \2                     # closing quote
+    :                      # colon
+    [^\S\n\r]*             # any non-line-end whitespace
+    (\#.*)?                # maybe a comment
+    $
+    ''',
+    flags=re.VERBOSE)
+
+ROUTE_BLOCK_BODY = re.compile(
+    r'''
+    ^(\s*)                  # indentation
+    (?P<key>\S+)\s*         # Ruby hash key
+    =>\s*                   # Ruby hash arrow
+    ([\'"]?)                # maybe quotes
+    (?P<value>.*?)          # Ruby hash value
+    \3                      # closing quote
+    ,?                      # maybe a comma
+                            # (commas are optional for the last item)
+    [^\S\n\r]*              # any non-line-end whitespace
+    (\#.*)?                 # maybe a comment
+    $
+    ''',
+    flags=re.VERBOSE)
+
+CLOSE_BRACE = re.compile(
+    r'''
+    ^(\s*)          # indentation
+    }               # closing brace
+    [^\S\n\r]*      # any non-line-end whitespace
+    (\#.*)?         # maybe a comment
+    $
+    ''',
+    flags=re.VERBOSE)
+
+
+class RouteParserError(core.RouteError):
+    pass
+
+
+class StartTokenNotFoundError(RouteParserError):
+    pass
+
+
+class EndTokenNotFoundError(RouteParserError):
+    pass
+
+
+class RouteParser(object):
+    """Route parser."""
+
+    def __init__(self, lines, file_header=ROUTE_FILE_HEADER,
+                 block_head=ROUTE_BLOCK_HEAD, block_body=ROUTE_BLOCK_BODY,
+                 block_close=CLOSE_BRACE):
+        self._lines = lines
+        self._file_header = file_header
+        self._block_head = block_head
+        self._block_body = block_body
+        self._block_close = block_close
+
+    def _find_file_header(self):
+        """Find the file header."""
+        for line in self._lines:
+            if self._file_header.match(line):
+                break
+        else:
+            raise StartTokenNotFoundError('No match for file header.')
+
+    def _find_close_brace(self):
+        """Find the close brace."""
+        for line in self._lines:
+            if self._block_close.match(line):
+                break
+        else:
+            raise EndTokenNotFoundError('No match for close brace.')
+
+    def _find_block_start(self):
+        """Find the beginning of a code block."""
+        for line in self._lines:
+            head_match = self._block_head.match(line)
+            if head_match:
+                return head_match.groupdict()
+        raise StartTokenNotFoundError('No match for code block start.')
+
+    def parse_one(self):
+        """Find a route block in an iterable of strings (e.g. lines in file).
+
+        Example:
+
+        >>> with open('my_route_file.pp') as route_file:
+        ...     parser = RouteParser(route_file)
+        ...     route = parser.parse_one()
+        ...     print(route)
+
+        """
+        route = self._find_block_start()
+        # Code block body parser
+        for line in self._lines:
+            if self._block_close.match(line):
+                break
+            block_item_match = self._block_body.match(line)
+            if block_item_match:
+                item = block_item_match.groupdict()
+                # NOTE: 'key' and 'value' named capture groups are expected
+                # in the regular expression pattern
+                key, value = item['key'], item['value']
+                route[key] = value
+        else:
+            raise EndTokenNotFoundError('No match for code block end.')
+        return route
+
+    def parse_all(self):
+        """Itertively parse all routes in an iterable of strings (e.g. a file).
+
+        Example:
+
+        >>> with open('my_route_file.pp') as route_file:
+        ...     parser = RouteParser(route_file)
+        ...     for route in parser.parse_all():
+        ...         print(route)
+
+        """
+        try:
+            self._find_file_header()
+            while True:
+                yield self.parse_one()
+        except StartTokenNotFoundError:
+            return
---
+++ tests/test_parser.py
@@ -0,0 +1,214 @@
+# -*- coding: utf-8 -*-
+
+"""Route parser tests."""
+
+from __future__ import (
+    absolute_import,
+    print_function,
+    unicode_literals,
+    with_statement,
+)
+
+import unittest
+
+try:
+    from io import StringIO
+except ImportError:
+    # NOTE: PY2 compat
+    from cStringIO import StringIO
+
+from route_ctl.parser import (
+    RouteParser,
+    StartTokenNotFoundError,
+    EndTokenNotFoundError,
+)
+
+
+VALID_ROUTE_FILE = '''\
+# a comment
+class netroutes::routes {               # comment
+  network_route { '172.17.67.0/24':     # comment
+    ensure    => 'present',             # comment
+    gateway   => '10.0.2.2',            # comment
+    interface => 'eth0',                # comment
+    netmask   => '255.255.255.0',       # comment
+    network   => '172.17.67.0',         # comment
+    options   => 'table 200',           # comment
+  }  # comment
+  network_route { 'default':
+    ensure    => 'present',
+    # ensure    => 'absent',
+    gateway   => '10.0.2.2',
+    # gateway   => '10.0.3.3',
+    interface => $appout,
+    netmask   => '0.0.0.0',
+    network   => 'default'
+  }  # end route
+}  # end file
+'''
+
+SINGLE_VALID_ROUTE_FILE = '''\
+# a comment
+class netroutes::routes {               # comment
+  network_route { '172.17.67.0/24':     # comment
+    ensure    => 'present',             # comment
+    gateway   => '10.0.2.2',            # comment
+    interface => 'eth0',                # comment
+    netmask   => '255.255.255.0',       # comment
+    network   => '172.17.67.0',         # comment
+    options   => 'table 200',           # comment
+  }  # comment
+}
+'''
+
+
+MISSING_HEADER_FILE = '''\
+# a comment
+network_route { '172.17.67.0/24':     # comment
+  ensure    => 'present',             # comment
+  gateway   => '10.0.2.2',            # comment
+  interface => 'eth0',                # comment
+  netmask   => '255.255.255.0',       # comment
+  network   => '172.17.67.0',         # comment
+  options   => 'table 200',           # comment
+}  # comment
+network_route { 'default':
+  ensure    => 'present',
+  # ensure    => 'absent',
+  gateway   => '10.0.2.2',
+  # gateway   => '10.0.3.3',
+  interface => $appout,
+  netmask   => '0.0.0.0',
+  network   => 'default'
+}  # end route
+'''
+
+MISSING_OPEN_BRACE_FILE = '''\
+# a comment
+class netroutes::routes                 # comment
+  network_route   '172.17.67.0/24':     # comment
+    ensure    => 'present',             # comment
+    gateway   => '10.0.2.2',            # comment
+    interface => 'eth0',                # comment
+    netmask   => '255.255.255.0',       # comment
+    network   => '172.17.67.0',         # comment
+    options   => 'table 200',           # comment
+} # end file
+'''
+
+MISSING_CLOSE_BRACE_FILE = '''\
+# a comment
+class netroutes::routes {               # comment
+  network_route { '172.17.67.0/24':     # comment
+    ensure    => 'present',             # comment
+    gateway   => '10.0.2.2',            # comment
+    interface => 'eth0',                # comment
+    netmask   => '255.255.255.0',       # comment
+    network   => '172.17.67.0',         # comment
+    options   => 'table 200',           # comment
+# end file
+'''
+
+VALID_ROUTES = [
+    {
+        'name': '172.17.67.0/24',
+        'ensure': 'present',
+        'gateway': '10.0.2.2',
+        'interface': 'eth0',
+        'netmask': '255.255.255.0',
+        'network': '172.17.67.0',
+        'options': 'table 200',
+    },
+    {
+        'name': 'default',
+        'ensure': 'present',
+        'gateway': '10.0.2.2',
+        'interface': '$appout',
+        'netmask': '0.0.0.0',
+        'network': 'default',
+    }
+]
+
+
+class TestFindHeader(unittest.TestCase):
+    """Test `find_header()` function."""
+
+    def test_header_present(self):
+        """Should stop right after the header is found."""
+        with StringIO(VALID_ROUTE_FILE) as route_file:
+            parser = RouteParser(route_file)
+            try:
+                parser._find_file_header()
+            except Exception as err:
+                self.fail(err)
+            line = route_file.readline().rstrip()
+            # should read the first line after the header
+            self.assertEqual(
+                line, "  network_route { '172.17.67.0/24':     # comment")
+
+    def test_header_absent(self):
+        """Should raise the correct exception if header doesn't exist."""
+        with StringIO(MISSING_HEADER_FILE) as route_file:
+            parser = RouteParser(route_file)
+            # NOTE: there is no context manager for ``assertRaises``` in PY26.
+            self.assertRaises(StartTokenNotFoundError, parser._find_file_header)
+
+
+class TestFindClosingBrace(unittest.TestCase):
+    """Test `find_closing_brace` function."""
+
+    def test_closing_brace_present(self):
+        """Should raise no exceptions if a closing brace is found."""
+        with StringIO(VALID_ROUTE_FILE) as route_file:
+            parser = RouteParser(route_file)
+            try:
+                parser._find_close_brace()
+            except Exception as err:
+                self.fail(err)
+
+    def test_closing_brace_absent(self):
+        """Correct exception should be raised when missing a close brace."""
+        with StringIO(MISSING_CLOSE_BRACE_FILE) as route_file:
+            parser = RouteParser(route_file)
+            # NOTE: there is no context manager for ``assertRaises``` in PY26.
+            self.assertRaises(EndTokenNotFoundError, parser.parse_one)
+
+
+class TestParseRoute(unittest.TestCase):
+    """Test `parse_route` function."""
+
+    def test_one_route_present(self):
+        """Should correctly return the parsed route."""
+        with StringIO(SINGLE_VALID_ROUTE_FILE) as route_file:
+            parser = RouteParser(route_file)
+            self.assertEqual(parser.parse_one(), VALID_ROUTES[0])
+
+    def test_many_routes_present(self):
+        """Should correctly return the first parsed route."""
+        with StringIO(VALID_ROUTE_FILE) as route_file:
+            parser = RouteParser(route_file)
+            self.assertEqual(parser.parse_one(), VALID_ROUTES[0])
+
+    def test_missing_close_brace(self):
+        """Should raise the correct exception when missing a close brace."""
+        with StringIO(MISSING_CLOSE_BRACE_FILE) as route_file:
+            parser = RouteParser(route_file)
+            # NOTE: there is no context manager for ``assertRaises``` in PY26.
+            self.assertRaises(EndTokenNotFoundError, parser.parse_one)
+
+    def test_missing_open_brace(self):
+        """Should raise then correct exception when missing a close brace."""
+        with StringIO(MISSING_OPEN_BRACE_FILE) as route_file:
+            parser = RouteParser(route_file)
+            # NOTE: there is no context manager for ``assertRaises``` in PY26.
+            self.assertRaises(StartTokenNotFoundError, parser.parse_one)
+
+
+class TestParseRoutes(unittest.TestCase):
+    """Perform route parsing tests."""
+
+    def test_parse_all_routes(self):
+        """Should parse all routes correctly."""
+        with StringIO(VALID_ROUTE_FILE) as route_file:
+            parser = RouteParser(route_file)
+            self.assertEqual(list(parser.parse_all()), VALID_ROUTES)
